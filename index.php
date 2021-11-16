6b487c56802a3d1d67ee7382ac2353a240fce0e9
Rafael <rafaelpianaro@gmail.com>
2021-11-15 21:56:25 -0300

    *campos *validando campos vazios

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistema de Login</title>
 </head>
 <body>
+
+    <?php
+        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if(!empty($dados['SendLogin'])){
+            var_dump($dados);
+        }
+    ?>
+
     <h1>Login</h1>
+    <form action="" method="POST">
+        <label for="">Usuário</label>
+        <input type="text" name="usuario" placeholder="Digite o usuário"><br><br>
+        <label for="">Senha</label>
+        <input type="password" name="senha_usuario" placeholder="Digite a senha"><br><br>
+        <input type="submit" name="SendLogin" id="" value="acessar">
+    </form>
 </body>
 </html>
