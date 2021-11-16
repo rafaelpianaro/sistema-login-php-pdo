a0ddc9273dbc3c4998ce4b54ca611c7ea1a563b3
Rafael <rafaelpianaro@gmail.com>
2021-11-15 23:01:10 -0300

    *verificar usuario em db *validar usuario

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     include_once 'conexao.php';
 ?>
 <!DOCTYPE html>
@@ -19,6 +20,22 @@
         $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         if(!empty($dados['SendLogin'])){
             var_dump($dados);
+            $query_usuario = "select id, usuario, senha_usuario from usuarios 
+                                where usuario = :usuario 
+                                limit 1";
+            $result_usuario = $conn->prepare($query_usuario);
+            $result_usuario->bindParam(':usuario',$dados['usuario'], PDO::PARAM_STR);
+            $result_usuario->execute();
+            if(($result_usuario) and ($result_usuario->rowCount() != 0)){
+                $row_usuario = $result_usuario->fetch(PDO::FETCH_ASSOC);
+                var_dump($row_usuario);
+            }else{
+                $_SESSION['msg'] = "<p style='color: red'>Erro: Usuario ou senha inválida.</p>";
+            }
+        }
+        if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+            unset($_SESSION['msg']);
         }
     ?>
 
